ec59f1bbd223d159bfb398abd4dc9e08664209f2
Alex Cruice <alex.cruice@gmail.com>
2016-03-16 12:40:27 +1000

    Drop path-edit.sh

--- shell/profile.sh
+++ shell/profile.sh
@@ -6,8 +6,7 @@ export EDITOR='vim -f'
 export HISTCONTROL=ignorespace:erasedups
 export HISTIGNORE='ls:lsl:bg:gf:history:exit'
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
-path_front '/usr/local/bin' '/usr/local/sbin'
-export PATH
+export PATH=/usr/local/bin:/usr/local/sbin:$PATH
 
 source "$(brew --prefix)/etc/bash_completion"
 source '/usr/local/opt/chruby/share/chruby/chruby.sh'
