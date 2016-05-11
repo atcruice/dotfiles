93dbd2b59f0e33cb2cbc6286ad0b2d67ee338d21
Alex Cruice <alex.cruice@gmail.com>
2016-05-11 15:07:04 +1000

    Add TRAVELLING_RUBY_PACKAGE_DIR environment variable

--- shell/profile.sh
+++ shell/profile.sh
@@ -8,6 +8,7 @@ export HISTIGNORE='ls:bg:gf:history:exit'
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 export PATH=/usr/local/bin:/usr/local/sbin:$PATH
 export PKG_CONFIG_PATH=/usr/X11/lib/pkgconfig:$PKG_CONFIG_PATH
+export TRAVELLING_RUBY_PACKAGE_DIR="$HOME/src"
 
 source "$(brew --prefix)/etc/bash_completion"
 source '/usr/local/opt/chruby/share/chruby/chruby.sh'
