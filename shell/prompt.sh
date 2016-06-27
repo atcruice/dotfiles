24efa343bbb63d1584a043f6322bcb6ce8fea05b
Alex Cruice <alex.cruice@gmail.com>
2016-06-28 09:09:35 +1000

    Change git-dirty prompt marker

--- shell/prompt.sh
+++ shell/prompt.sh
@@ -84,7 +84,7 @@ function git_info {
 
   # check if it's dirty (via github.com/sindresorhus/pure)
   local dirty
-  dirty=$(git diff --quiet --ignore-submodules HEAD &>/dev/null; [ $? -eq 1 ]&& echo -e "≠")
+  dirty=$(git diff --quiet --ignore-submodules HEAD &>/dev/null; [ $? -eq 1 ]&& echo -e "*")
 
   echo "\[$FG_BLACK\]\[$BG_YELLOW\] $branch$dirty \[$RESET\]"
 }
