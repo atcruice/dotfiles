6ed2d6f630035f71d320d9f15a232a075faf1282
Alex Cruice <alex.cruice@gmail.com>
2017-12-28 10:33:12 +1000

    Add _has shell function

--- shell/functions.sh
+++ shell/functions.sh
@@ -22,3 +22,14 @@ complete -o default -F _pip_completion pip
 function gemscrub {
     gem pristine --all --env-shebang --no-extensions
 }
+
+# credit https://www.bhalash.com/archives/13544809048
+function _has {
+    for PROGRAM in "$@"; do
+        if [[ $(which "$PROGRAM" &>/dev/null; echo $?) != 0 ]]; then
+            return 1
+        fi
+    done
+
+    return 0
+}
