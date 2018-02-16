6fad2358521705395fabb8867ea6ce64aeb68d01
Alex Cruice <alex.cruice@gmail.com>
2018-02-17 04:42:39 +1000

    Add GOPATH environment variable

--- shell/profile.sh
+++ shell/profile.sh
@@ -5,12 +5,14 @@ shopt -s cdspell histappend no_empty_cmd_completion
 
 export CLICOLOR=1
 export EDITOR='vim'
+export GOPATH
+GOPATH=$(go env GOPATH)
 export HISTCONTROL=ignorespace:erasedups
 export HISTIGNORE='ls:bg:gf:history:exit'
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 export LESS="--ignore-case --squeeze-blank-lines --LONG-PROMPT --RAW-CONTROL-CHARS"
 export PAGER="less"
-export PATH=/usr/local/bin:/usr/local/sbin:$PATH
+export PATH=/usr/local/bin:/usr/local/sbin:$PATH:$GOPATH/bin
 export PKG_CONFIG_PATH=/usr/X11/lib/pkgconfig:$PKG_CONFIG_PATH
 export TRAVELLING_RUBY_PACKAGE_DIR="$HOME/src"
 export GPG_TTY
