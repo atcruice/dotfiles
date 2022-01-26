d3722267bce615eeff5aa1c0963084c3beb3c3f8
Alex Cruice <alex.cruice@gmail.com>
2022-01-27 06:43:45 +1000

    Update CDPATH for new GitHub organisation

--- programs/bash/sensible_overrides.sh
+++ programs/bash/sensible_overrides.sh
@@ -1,4 +1,4 @@
 # These can't go in programs.bash.sessionVariables because
 # programs.bash.initExtra is assembled later
-CDPATH=".:~:~/code/fivegoodfriends:~/code/atcruice"
+CDPATH=".:~:~/code/thelookoutway:~/code/atcruice"
 PROMPT_COMMAND="$PROMPT_COMMAND;update_terminal_cwd"
