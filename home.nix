e0948b48598c180d4d1bf7e60986f97f572b656f
Alex Cruice <alex@thelookoutway.com>
2022-07-25 07:57:50 +1000

    Bump home-manager state version

--- home.nix
+++ home.nix
@@ -75,7 +75,7 @@ in {
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "21.03";
+  home.stateVersion = "22.05";
 
   home.sessionPath = [
     "$HOME/bin"
