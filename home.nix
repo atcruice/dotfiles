b74d84e4c17e77e1f3170026ed9665991e75b690
Alex Cruice <alex.cruice@gmail.com>
2022-07-25 16:23:39 +1000

    Update NIX_PATH
    According to home-manager installation guide.

--- home.nix
+++ home.nix
@@ -152,7 +152,7 @@ in {
       EDITOR = "vim";
       GPG_TTY = "$(tty)";
       LESS = "--ignore-case --squeeze-blank-lines --LONG-PROMPT --RAW-CONTROL-CHARS";
-      NIX_PATH = "$HOME/.nix-defexpr/channels\${NIX_PATH:+:}$NIX_PATH";
+      NIX_PATH = "$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels\${NIX_PATH:+:$NIX_PATH}";
       PAGER = "less";
     };
   };
