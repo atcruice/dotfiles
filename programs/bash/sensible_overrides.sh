869ddbcc919c3cfedb50c501aea719ec1377ea8c
Alex Cruice <alex.cruice@gmail.com>
2023-06-01 05:52:28 +1000

    Remove PROMPT_COMMAND customisation

--- programs/bash/sensible_overrides.sh
+++ programs/bash/sensible_overrides.sh
@@ -1,4 +1,3 @@
 # These can't go in programs.bash.sessionVariables because
 # programs.bash.initExtra is assembled later
 CDPATH=".:~:~/code/thelookoutway:~/code/atcruice"
-PROMPT_COMMAND="$PROMPT_COMMAND;update_terminal_cwd"
